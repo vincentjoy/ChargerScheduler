5dcf3b9ecf6f155759c3f9550da815cd934fac94
Vincent Joy <vincentjoyhere@gmail.com>
2025-08-02 09:53:16 +0530

    Initial Commit

---
+++ ChargerScheduler/ChargerSchedulerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChargerSchedulerApp.swift
+//  ChargerScheduler
+//
+//  Created by Vincent Joy on 02/08/25.
+//
+
+import SwiftUI
+
+@main
+struct ChargerSchedulerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
